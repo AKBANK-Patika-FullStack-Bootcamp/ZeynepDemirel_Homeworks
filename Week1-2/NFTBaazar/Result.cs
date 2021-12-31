b77a9cff15498b873dd066f11f738f219ac6ff28
Zeynep Demirel <56252190+zeynep-dmrl@users.noreply.github.com>
2021-12-31 15:22:06 +0300

    Result Class

---
+++ Week1-2/NFTBaazar/Result.cs
@@ -0,0 +1,10 @@
+﻿namespace NFTBaazar
+{
+    public class Result
+    {
+
+        public int status { get; set; }
+        public string message { get; set; }
+        public List<NFTtoken> list { get; set; }
+    }
+}
