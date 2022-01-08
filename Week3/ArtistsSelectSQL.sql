c9aa31e30eec1af0a7482ee34b0984c74a10698f
Zeynep Demirel <56252190+zeynep-dmrl@users.noreply.github.com>
2022-01-09 01:34:12 +0300

    Add files via upload

---
+++ Week3/ArtistsSelectSQL.sql
@@ -0,0 +1,11 @@
+USE [NFTBaazar]
+GO
+
+SELECT [artist_id]
+      ,[artist_firstName]
+      ,[artist_lastName]
+  FROM [dbo].[Artists]
+
+GO
+
+
---
+++ Week3/NFTtokensSelectSQL.sql
@@ -0,0 +1,13 @@
+USE [NFTBaazar]
+GO
+
+SELECT [tokenId]
+      ,[tokenName]
+      ,[dateOfCreate]
+      ,[price]
+      ,[artist_id]
+  FROM [dbo].[NFTtokens]
+
+GO
+
+
